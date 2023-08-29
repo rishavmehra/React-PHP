b59005cdafaae0f517a5c78359d45efb8e80bd80
rishavmehra <rishavmehra61@gmail.com>
2023-08-30 02:29:47 +0530

    fixes

--- basic.sh
+++ basic.sh
@@ -4,14 +4,15 @@
 sudo apt update
 sudo apt install curl -y
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-source ~/.bashrc
+# Install npm
+sudo apt install npm -y
 
-# Install Node.js 12.1.0 using NodeSource repository
+# Install Node.js 12.22.9 using NodeSource repository
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt install nodejs -y
 
 # Check Node.js and npm versions
 node -v
+npm -v
 
 echo "Node.js 12.22.9 and npm installation completed."
